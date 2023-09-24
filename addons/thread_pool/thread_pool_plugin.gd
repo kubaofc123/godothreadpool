231954b1cfb0495335b3e9091d1a7a1866a38367
Jakub Brzyski <blaqjack2222@gmail.com>
2023-09-24 11:22:46 +0200

    Update thread_pool_plugin.gd

--- addons/thread_pool/thread_pool_plugin.gd
+++ addons/thread_pool/thread_pool_plugin.gd
@@ -1,8 +1,12 @@
 @tool
 extends EditorPlugin
 
+# Replace this value with a PascalCase autoload name, as per the GDScript style guide.
+const AUTOLOAD_NAME = "GlobalThreadPool"
+
 func _enter_tree():
-	pass
+	# The autoload can be a scene or script file.
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/thread_pool/thread_pool.gd")
 
 func _exit_tree():
-	pass
+	remove_autoload_singleton(AUTOLOAD_NAME)
